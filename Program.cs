f38aa80d107f5f0d42e9782d903fc501d04dcbfa
rafaelbasler07 <rafael.basler@bbv.ch>
2024-06-12 16:11:43 +0200

    parameter handling

--- Program.cs
+++ Program.cs
@@ -1,24 +1,60 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace BucherFussballLiga
 {
   internal class Program
   {
-    // args[0] - the filename in the format of "dayxx" (optional)
-    // args[1] - sorting criteria      
+    public enum SortCriteria
+    { 
+      Points, 
+      GoalDifference, 
+      NumberOfWins, 
+      Name,
+      NULL
+    }
+    // Main method with possible parameters
+    // args[0] - the filename in the format of "dayXX" 
+    // args[1] - sorting criteria (optional, default is points)   
     static void Main(string[] args)
     {
+      //check/read parameters
       string fileName = args.Length > 0 ? args[0] + ".txt" : null;
-      string sortCriteria = args.Length > 1 ? args[1] : "pointsDesc";
+      string pattern = @"^day\d{2}$";
+      if(fileName == null || !Regex.IsMatch(args[0], pattern))
+      {
+        Environment.Exit(0);
+      }
+
+      SortCriteria sortCriteria = SortCriteria.NULL;
+      if(args.Length == 2)
+      {
+        switch(args[1])
+        {
+          case "1":
+            sortCriteria = SortCriteria.Points;
+            break;
+          case "2":
+            sortCriteria = SortCriteria.GoalDifference;
+            break;
+          case "3":
+            sortCriteria = SortCriteria.NumberOfWins;
+            break;
+          case "4":
+            sortCriteria =SortCriteria.Name;
+            break;
+          default:
+            throw new Exception("Invalid sorting criteria!");
+        }
+      }
 
       var leagueTable = new LeagueTable();
       leagueTable.ProcessFiles(fileName);
       leagueTable.SortAndDisplayTable(sortCriteria);
-
     }
   }
 }
